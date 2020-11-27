28c3760bd7cfefdbbb27e1c6af3952df14187ed5
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2020-11-27 01:45:01 -0700

    Player Animation Implemented
    Run and Jump Animation implemented, changed graphics to knight.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,16 +1,8 @@
 extends Node2D
 
+onready var animation  = $AnimatedSprite #onready - means wait until scene is ready, Gets animated sprite of Player
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animation.play("Run")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
