6f8bea32db83db325e2f6ff6436ef50548390dc7
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2020-11-27 02:47:43 -0700

    Improve player actions Finite State Machines
    JUMP, RUN, IDLE States

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,34 +5,45 @@ var velocity = Vector2.ZERO
 export var jump_velocity = 650.0	#export variables get displayed in inspector when in Player scene
 export var gravity_scale = 25.0
 
-var can_jump = true
+#Finite State Machines
+enum {
+	JUMP,
+	RUN,
+	IDLE
+}
+
+var state = RUN
 
 onready var animation  = $AnimatedSprite #onready - means wait until scene is ready, Gets animated sprite of Player
 
-func _ready():
-	animation.play("Run")
-
+#FINITE STATE MACHINES -- simplify way code works, transitioning between states
 func _physics_process(delta):
+	match state:
+		RUN:
+			animation.play("Run")	#play run animation
+		JUMP:
+			velocity = Vector2.ZERO
+			velocity.y -= jump_velocity
+			animation.play("Jump")
+			state = IDLE	#set state to IDLE right away due to Jumping only for an instant, wait for player to get to the ground
+		IDLE:
+			pass
+	
+	
 	velocity.y += gravity_scale #2D, y direction positive is down, negative is upwards, to make player fall
 	move_and_collide(velocity*delta) #make frame independent, scaled via time or framerate
 
 #detect any input that comes in its own little loop of Godot Ecosystem.
 #for events and button input
 func _input(event):
-	if can_jump:
-		velocity = Vector2.ZERO
+	if state == RUN:
 		if event.is_action_pressed("Jump"):
-			velocity.y -= jump_velocity
-			animation.play("Jump")
-
-
+			state = JUMP
 
 func _on_Area2D_body_entered(body):
 	if body is StaticBody2D: #StaticBody2d Foreground, when player touches foreground floor, execute code underneath
-		can_jump = true		#Allow jump because grounded
-		animation.play("Run")	#play run animation
-
+		state = RUN
 
 func _on_Area2D_body_exited(body):
 	if body is StaticBody2D:	#Block Jump
-		can_jump = false
+		state = JUMP
