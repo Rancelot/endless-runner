2be821dc6ba7c4065bca2bffbd056bb57ee5e8b5
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2020-12-02 19:56:55 -0700

    Leveling update - Spawner
    spawner will increase spawn rate after certain scores are reached, will balance as I go.

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -4,7 +4,7 @@ export (Array, PackedScene) var scenes
 
 var random_scene = RandomNumberGenerator.new()
 var selected_scene_index = 0
-var spawn_waittime = 3
+var spawn_waittime = 3.5
 onready var spawner = $Timer
 
 func _on_Timer_timeout():
@@ -12,7 +12,12 @@ func _on_Timer_timeout():
 	selected_scene_index = random_scene.randi_range(0, scenes.size()-1)		#randi_range -- random integer from 0 to length of array - 1
 	var tmp = scenes[selected_scene_index].instance()
 	add_child_below_node(self, tmp)		#add as child node to current scene which is a spawner
-	spawn_waittime -= 0.3
+	
+	if Constants.score > 4:
+		spawn_waittime -= 0.1
+	elif Constants.score > 10:
+		spawn_waittime -= 0.2
+	
 	print(spawn_waittime)
 	if spawn_waittime < 1.0:
 		spawn_waittime = 1.0
