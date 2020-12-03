da0c1634de9974cb56cf3cd0e91d438f40516cae
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2020-12-03 00:45:32 -0700

    Update TimeElapsedUI.gd

--- Scripts/TimeElapsedUI.gd
+++ Scripts/TimeElapsedUI.gd
@@ -3,8 +3,6 @@ extends RichTextLabel
 func _ready():
 	Constants.time = 0
 	Signals.connect("stopTimer", self, "stopTimer")
-#	time_start = OS.get_unix_time()
-#	set_process(true)
 
 func _process(delta):
 	if Constants.timer_playing:
@@ -12,12 +10,6 @@ func _process(delta):
 		var seconds = fmod(Constants.time, 60)
 		var minutes = fmod(Constants.time, 3600) / 60
 		var str_elapsed = "%02d : %02d" % [minutes, seconds]
-#	time_now = OS.get_unix_time()
-#	elapsed = time_now - time_start
-#	var minutes = elapsed / 60
-#	var seconds = elapsed % 60
-#	var str_elapsed = "%02d : %02d" % [minutes, seconds]
-		print("elapsed: ", str_elapsed)
 		self.text = str_elapsed
 
 func stopTimer():
