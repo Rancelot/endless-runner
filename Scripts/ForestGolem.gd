6c0fa5cd6ae127ee57541bc10bfd04e40dc841fb
Rafael Angelo Pucut <38448911+Rancelot@users.noreply.github.com>
2020-12-02 23:10:02 -0700

    New Enemy and updated Spawner
    golem enemy and change level scaling for spawner

--- Scripts/ForestGolem.gd
+++ Scripts/ForestGolem.gd
@@ -1,8 +1,12 @@
 extends "ScrollMovement.gd"
 
+onready var animation = $AnimatedSprite
+
+func _ready():
+	animation.play("Run")
+
 func _physics_process(delta):
 	move()
-
 
 func _on_EffectDamage_body_entered(body):
 	if body.name == "Player": #player node, if hits player, launch code
--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -4,7 +4,7 @@ export (Array, PackedScene) var scenes
 
 var random_scene = RandomNumberGenerator.new()
 var selected_scene_index = 0
-var spawn_waittime = 3.5
+var spawn_waittime = 2.5
 onready var spawner = $Timer
 
 func _on_Timer_timeout():
@@ -14,9 +14,11 @@ func _on_Timer_timeout():
 	add_child_below_node(self, tmp)		#add as child node to current scene which is a spawner
 	
 	if Constants.score >= 10:
-		spawn_waittime -= 0.2
+		spawn_waittime = 1.3
+#		spawn_waittime -= 0.2
 	elif Constants.score >= 4:
-		spawn_waittime -= 0.1
+		spawn_waittime = 1.7
+#		spawn_waittime -= 0.1
 	
 	print(spawn_waittime)
 	if spawn_waittime < 1.0:
